ecab71567121f28a45df0430b7ccba8f2f7b9ff2
Marcin M <mircin27@gmail.com>
2022-08-26 20:06:26 +0200

    Added crud to Controllers

--- src/main/java/com/example/conferencedemo/controllers/SessionController.java
+++ src/main/java/com/example/conferencedemo/controllers/SessionController.java
@@ -2,6 +2,7 @@ package com.example.conferencedemo.controllers;
 
 import com.example.conferencedemo.models.Session;
 import com.example.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,5 +24,23 @@ public class SessionController {
     @RequestMapping("{id}")
     public Session get(@PathVariable Long id){
         return sessionRepository.getOne(id);
+    }
+    @PostMapping
+    public Session create(@RequestBody final Session session){
+        return sessionRepository.saveAndFlush(session);
+    }
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        //Check for children records before deleting.
+        sessionRepository.deleteById(id);
+    }
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session){
+        //PUT all attribute passed in, A PATCH need only what we want to change.
+        //TODO need validation that all attribute are passed in, otherwise return a 400 bad payload
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session,existingSession, "session_id"); //Need to ignore or because of primary key
+        return sessionRepository.saveAndFlush(existingSession);
+
     }
 }
--- src/main/java/com/example/conferencedemo/controllers/SpeakerController.java
+++ src/main/java/com/example/conferencedemo/controllers/SpeakerController.java
@@ -2,10 +2,9 @@ package com.example.conferencedemo.controllers;
 
 import com.example.conferencedemo.models.Speaker;
 import com.example.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +17,28 @@ public class SpeakerController {
     @GetMapping
     public List<Speaker> list(){
         return speakerRepository.findAll();
+    }
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getOne(id);
+    }
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        //children
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        //TODO validation
+        BeanUtils.copyProperties(speaker , existingSpeaker,"speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
     }
 }
